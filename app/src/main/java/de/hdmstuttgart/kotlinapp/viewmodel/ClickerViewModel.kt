a2837a91d8d84cbe16ae824ee83afcc03dd9a3a5
David Zack Walker <davidzzwalker@gmail.com>
2019-11-09 16:06:19 +0100

    add textview for clicks per sec

--- app/src/main/java/de/hdmstuttgart/kotlinapp/viewmodel/ClickerViewModel.kt
+++ app/src/main/java/de/hdmstuttgart/kotlinapp/viewmodel/ClickerViewModel.kt
@@ -6,11 +6,26 @@ import androidx.databinding.library.baseAdapters.BR
 import de.hdmstuttgart.kotlinapp.model.AutoClickers
 import de.hdmstuttgart.kotlinapp.model.IAutoClicker
 import de.hdmstuttgart.kotlinapp.util.AutoClickerFactory
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class ClickerViewModel : BaseObservable() {
 
     @Bindable
     var clicks = 0
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.clicks)
+        }
+
+    @Bindable
+    var clicksPerSec = 0.0
+        set(value) {
+            val df = DecimalFormat("#.#")
+            df.roundingMode = RoundingMode.CEILING
+            field = df.format(value).toDouble()
+            notifyPropertyChanged(BR.clicksPerSec)
+        }
 
     private val clickers = mutableListOf<IAutoClicker>()
 
@@ -21,13 +36,11 @@ class ClickerViewModel : BaseObservable() {
     fun addClick()
     {
         clicks++
-        notifyPropertyChanged(BR.clicks)
     }
 
     fun addClicks(amount : Int)
     {
         clicks += amount
-        notifyPropertyChanged(BR.clicks)
     }
 
     fun removeClicks(amount : Int)
@@ -44,6 +57,7 @@ class ClickerViewModel : BaseObservable() {
             removeClicks(clicker.price)
             clickers.add(clicker)
             Thread(clicker).start()
+            clicksPerSec += clicker.clicksPerSecond
         }
     }
 
