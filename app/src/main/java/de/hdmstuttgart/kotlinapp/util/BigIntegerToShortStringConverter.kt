9050c07a77ea8cb42646e781a5b105a4421115e0
David Zack Walker <davidzzwalker@gmail.com>
2019-11-10 20:31:38 +0100

    Update BigIntegerToShortStringConverter.kt

--- app/src/main/java/de/hdmstuttgart/kotlinapp/util/BigIntegerToShortStringConverter.kt
+++ app/src/main/java/de/hdmstuttgart/kotlinapp/util/BigIntegerToShortStringConverter.kt
@@ -1,10 +1,16 @@
 package de.hdmstuttgart.kotlinapp.util
 
 import java.math.BigDecimal
-import java.math.BigInteger
 
 class BigIntegerToShortStringConverter {
     companion object {
+
+        private val BigDecimal_OneThousand = BigDecimal(1000)
+        private val BigDecimal_OneHundredThousand = BigDecimal(100_000)
+        private val BigDecimal_OneMillion = BigDecimal(1_000_000)
+        private val BigDecimal_OneBillion = BigDecimal(1_000_000_000)
+        private val BigDecimal_OneTrillion = BigDecimal(1_000_000_000_000)
+        private val BigDecimal_OneAA = BigDecimal(1_000_000_000_000_000)
 
         private const val suffixThousand = "K"
         private const val suffixMillion = "M"
@@ -13,16 +19,16 @@ class BigIntegerToShortStringConverter {
 
         fun getStringForClicks(clicks : BigDecimal) : String {
             return when {
-                clicks < 100_000.toBigDecimal()
+                clicks < BigDecimal_OneHundredThousand
                     -> clicks.toString()
-                clicks < 1_000_000.toBigDecimal()
-                    -> String.format("%.2f", clicks / 1000.0.toBigDecimal()) + suffixThousand
-                clicks < 1_000_000_000.toBigDecimal()
-                    -> String.format("%.2f", clicks / 1_000_000.0.toBigDecimal()) + suffixMillion
-                clicks < 1_000_000_000_000.toBigDecimal()
-                    -> String.format("%.2f", clicks / 1_000_000_000.0.toBigDecimal()) + suffixBillion
-                clicks < 1_000_000_000_000_000.toBigDecimal()
-                    -> String.format("%.2f", clicks / 1_000_000_000_000.0.toBigDecimal()) + suffixTrillion
+                clicks < BigDecimal_OneMillion
+                    -> String.format("%.2f", clicks.divide(BigDecimal_OneThousand)) + suffixThousand
+                clicks < BigDecimal_OneBillion
+                    -> String.format("%.2f", clicks.divide(BigDecimal_OneMillion)) + suffixMillion
+                clicks < BigDecimal_OneTrillion
+                    -> String.format("%.2f", clicks.divide(BigDecimal_OneBillion)) + suffixBillion
+                clicks < BigDecimal_OneAA
+                    -> String.format("%.2f", clicks.divide(BigDecimal_OneTrillion)) + suffixTrillion
                 else -> "Over 9000"
             }
         }
